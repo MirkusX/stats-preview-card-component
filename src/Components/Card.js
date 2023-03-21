d8f6e0a6651a12fbfb580632c0583b1d35dc2808
MirkusX <marcusk.kodehode@gmail.com>
2023-03-21 11:20:57 +0100

    text and image added

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,8 +1,33 @@
+import image from "../Images/image.jpg";
+
 export const Card = () => {
   return (
     <section>
       <div>
-        <h1>card</h1>
+        <div>
+          <h1>Get insights that help your business grow.</h1>
+          <p>
+            Discover the benefits of data analytics and make better decisions
+            regarding revenue, customer experience, and overall efficiency.
+          </p>
+          <div>
+            <div>
+              <h2>10k+</h2>
+              <p>COMPANIES</p>
+            </div>
+            <div>
+              <h2>314</h2>
+              <p>TEMPLATES</p>
+            </div>
+            <div>
+              <h2>12M+</h2>
+              <p>QUERIES</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <img src={image} />
+        </div>
       </div>
     </section>
   );
